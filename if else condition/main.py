b96cf97e4d3959e4142331d82ad2acb5e2368096
tayyab404 <tayyabmunir464@gmail.com>
2025-08-04 21:10:11 +0500

    Add files via upload

---
+++ if else condition/main.py
@@ -0,0 +1,6 @@
+a=int(input("Enter your age: "))
+print("your ge is:",a)
+if(a>18):
+    print("yor arealigible for the driving")
+else:
+    print("you are not eligible for the vote")
---
+++ if else condition/number.py
@@ -0,0 +1,17 @@
+a=int(input(
+"Enter your number"
+))
+if a % 2 ==0:
+    print(" Your number is even:", a)
+else:
+    print("your number is odd:", a)
+
+b=float(input(" enter your number:"))
+
+if b>0:
+    print("number is positive:",b)
+elif b==0:
+    print("your number is zero:",b)
+else:
+    print("your number is negtive:",b)
+    
