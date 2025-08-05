4436d86776aacb55fe4db08540326756a5d8d984
tayyab404 <tayyabmunir464@gmail.com>
2025-08-05 18:46:52 +0500

    Add files via upload

---
+++ basics of python/break and continue in Python/main.py
@@ -0,0 +1,16 @@
+# for i in range(10):
+#     if(i==10):
+#         break
+#     print('5 *',i+1, "=",5*(i+1))
+# print(" Now you are out side the loop:")
+
+
+for i in range(15):
+    if(i==10):
+        print(" Now you are skip the iteration :")
+        continue
+    print('5 *',i, "=",5*i)
+ 
+
+
+ 
