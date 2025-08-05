ba40bbbb5961c7942c7b79de2fa21c44688236d0
tayyab404 <tayyabmunir464@gmail.com>
2025-08-05 18:28:57 +0500

    Add files via upload

---
+++ basics of python/while loop in python/main.py
@@ -0,0 +1,13 @@
+num=int(input("Enter the num value"))
+print( num)
+while(num<=38):
+ num=int(input(" enter your num:"))
+ print(num)
+print(" here is the end of loop") 
+
+# count=5
+# while(count>0):
+#     print(count)
+#     count=count-1
+# else:
+#     print(" now we are outside the while loop")
