6fd8b46865a1169b0e37919ba2eda05c5095d369
tayyab404 <tayyabmunir464@gmail.com>
2025-08-05 13:31:13 +0500

    Add files via upload

---
+++ for loops in python/main.py
@@ -0,0 +1,14 @@
+# name=' Tayyab'
+# for i in name:
+#     print(i)
+#     if (i=="y"):
+#         print(" that is the special chatracter in your name :")
+# departments=[" soft_engineering"," inf_technilogy","comp_science"]
+# for departments in departments:
+#     print(departments)
+#     for i in departments:
+#         print(i)
+for i in range(10):
+    print(i)
+for k in range(1,20000000001):
+    print(k)
