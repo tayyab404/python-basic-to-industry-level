f5f60d3381cbb33cff3d1dd41b4315590bd7b93e
tayyab404 <tayyabmunir464@gmail.com>
2025-08-11 15:37:55 +0500

    Add files via upload

---
+++ basics of python/lists/main.py
@@ -0,0 +1,26 @@
+marks=[1,5,7,9,"Tayyab",True]
+# print_list
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(marks[3])
+# print(marks[4])
+# print(marks[5])
+# print(marks[6])
+# -ve indexing
+# print(marks[-1])
+# print(marks[-5])
+# if "Tayyab" in marks:
+#     print("hmm")
+# else:
+#     print("naaa")
+
+
+# if "ab" in marks:
+#     print("yes")
+# else:
+#     print("No")
+print(marks[1:-2])
+print(marks[:])
+print(marks[2:4-5])
