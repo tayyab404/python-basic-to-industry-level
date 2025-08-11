68890276ad72358278123fb8e1601a89804db52f
tayyab404 <tayyabmunir464@gmail.com>
2025-08-11 16:17:41 +0500

    Add files via upload

---
+++ basics of python/metods in list/main.py
@@ -0,0 +1,16 @@
+L=[1,33,45,65,78,3,9,0,12,1,1,1,1]
+# print(L)
+# L.append(90000)
+# L.sort()
+# L.sort(reverse=True)
+# L.reverse()
+# print(L.index(1))
+# print(L.count(1))
+# m=L
+# m[0]=0
+# L.insert(3,600000000)
+m=[22,33,55,66,77,88,99,00]
+# L.extend(m)
+# p=L+m
+# print(p)
+print(L)
