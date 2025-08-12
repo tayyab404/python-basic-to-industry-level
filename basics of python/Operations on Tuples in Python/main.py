b19960d2ae903654290c45affccde5bd92f0385a
tayyab404 <tayyabmunir464@gmail.com>
2025-08-12 13:08:46 +0500

    Add files via upload

---
+++ basics of python/Operations on Tuples in Python/main.py
@@ -0,0 +1,16 @@
+# contry=("pakistan","Iran","UAE","Australia","UK")
+# country2=list(contry)
+# country2.append("Germny")
+# # print(country2)
+# country2.pop(2)
+# country2[2]="Italy"
+# country=tuple[country2]
+# print(country)
+# contry=("pakistan","Iran","UAE","Australia","UK")
+# contry2=("Usa","France","China","Russia")
+# merge=contry+contry2
+# print(merge)
+tup=(1,2,3333,4,5,6,6,5,4,3,2,1,4,5,6,2,7,3,2,5,6,3)
+# up=tup.count(3)
+up=tup.index(4)
+print(up)
