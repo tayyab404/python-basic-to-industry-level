ee85bb485075be0561a4c041cf379f3f79e77c2e
tayyab404 <tayyabmunir464@gmail.com>
2025-08-19 18:31:52 +0500

    Add files via upload

---
+++ basics of python/Docstrings in Python/main.py
@@ -0,0 +1,8 @@
+def square(n):
+    # doc string of the function
+    ''' this takes the input and print the outcome '''
+    print(n**2)
+square(10)
+print(square.__doc__)
+# PEP 8 mean python inhacement prposal
+import this
