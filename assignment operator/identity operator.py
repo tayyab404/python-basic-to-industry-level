13918fd448a84d3b5bba89964fa97e1286d749a4
tayyab404 <tayyabmunir464@gmail.com>
2025-10-08 15:04:07 +0500

    Add files via upload

---
+++ assignment operator/identity operator.py
@@ -0,0 +1,8 @@
+# This is use for the compare the memory location of the objects
+# is- always return true whe the memoe=ry location is same 
+# is not - return true when the memory location is not same 
+a=[1,2,3]
+b=a
+c=[1,2,3]
+print(a is b)
+print(a is c)
---
+++ assignment operator/membership operator.py
@@ -0,0 +1,5 @@
+# check the existance of a value ina asequence mean dic,tuple ,list 
+# in-true 
+# not in - true 
+vegtable=["bhindi", 'karala','aloo']
+print('aloo' in vegtable)
